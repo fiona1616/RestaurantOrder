e2a63b41b8e434618eb870905339f1f0a83773ac
Alan <alan.santoso@trimegah.com>
2023-12-14 09:53:48 +0700

    Initial Commit

---
+++ RestaurantOrder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestaurantOrder
+//
+//  Created by Fiona on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestaurantOrder/RestaurantOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantOrderApp.swift
+//  RestaurantOrder
+//
+//  Created by Fiona on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
